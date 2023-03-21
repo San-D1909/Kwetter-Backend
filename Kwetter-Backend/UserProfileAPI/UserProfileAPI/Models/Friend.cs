de62b3a781ceece3c670b9ccba5099e710b1d857
San-D1909 <75245672+San-D1909@users.noreply.github.com>
2023-03-21 14:36:15 +0100

    Create databases

---
+++ Kwetter-Backend/UserProfileAPI/UserProfileAPI/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿namespace UserProfileAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Friend
+    {
+        [Key]
+        public Guid FriendId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid FriendsWith { get; set; }
+
+    }
+}
--- Kwetter-Backend/UserProfileAPI/UserProfileAPI/Program.cs
+++ Kwetter-Backend/UserProfileAPI/UserProfileAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Polly;
+using UserProfileAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var cs = builder.Configuration.GetConnectionString("DefaultConnection")!;
+builder.Services.AddDbContext<ApplicationContext>(options =>
+options.UseMySql(cs, ServerVersion.AutoDetect(cs)));
+
 var app = builder.Build();
+
+// Create polly policy for database connection:
+var policy = Policy.Handle<Exception>().WaitAndRetryForever(
+    sleepDurationProvider: attempt => TimeSpan.FromMilliseconds(200), // Wait 200ms between each try.
+    onRetry: (exception, calculatedWaitDuration) => // Capture some info for logging.
+    {
+        Console.WriteLine("Could not connect to database, retrying");
+    });
+
+// Migrate latest database changes during startup
+using (var scope = app.Services.CreateScope())
+{
+    // Here is the migration executed inside the polly policy
+    policy.Execute(() =>
+    {
+        var dbContext = scope.ServiceProvider
+        .GetRequiredService<ApplicationContext>();
+
+        dbContext.Database.Migrate();
+    });
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
